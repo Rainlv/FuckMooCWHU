a90208c9400310d2694afd6fc0cabb47e3b8221f
Xuanh.W <Xuanhao.W.coding@outlook.com>
2022-08-02 09:27:45 +0800

    perf: improves performance

--- main.js
+++ main.js
@@ -1,52 +1,53 @@
 // ==UserScript==
-// @name         Fuck 安全教育
+// @name         Fuck 安全教育For WHU
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  狗都不学
-// @author       You
+// @version      0.2
+// @description  安全教育，狗都不学
+// @author       Rainnalv
+// @copyright    2022, Rainnalv (https://github.com/Rainlv)
+// @license      MIT
 // @match        http://mooc1.mooc.whu.edu.cn/mycourse/studentstudy*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=whu.edu.cn
 // @grant        none
 // ==/UserScript==
 
+// @homepage     https://github.com/Rainlv/FuckSafetyEducationWHU
+var player = null;
+var next_video_a_elem;
+
 var main = function () {
-  'use strict';
-
-  // 视频是否结束标志
-  var video_end_flag = false;
-
   // 播放器
-  var player = document.querySelector("iframe").contentWindow.
-  document.querySelector("iframe").contentWindow.
-  document.querySelector("#video_html5_api");
-  // 获取所有未完成列表
-  var todo_ls = document.getElementsByClassName("roundpointStudent orange01")
-  // 获取下一个视频elem
-  var next_video_a_elem = todo_ls[1].nextElementSibling
+  player = document.querySelector("iframe").contentWindow.document.querySelector("iframe").contentWindow.document.querySelector("#video_html5_api");
 
   // 监听第一次结束，跳转下个页面
   player.addEventListener('ended', function () {
-    video_end_flag = true;
     // 下一个视频
+    // 获取所有未完成列表
+    var todo_ls = document.getElementsByClassName("roundpointStudent orange01")
+    // 获取下一个视频a标签的elem
+    if (todo_ls.length === 0) return
+    if (todo_ls.length === 1)
+      next_video_a_elem = todo_ls[0].nextElementSibling
+    else
+      next_video_a_elem = todo_ls[1].nextElementSibling
+    player = null;
     next_video_a_elem.click();
+    // 等待15s，保证iframe加载完毕
+    setTimeout(function () {
+      main();
+    }, 15000)
   }, false);
 
   // 进入页面开始播放
   player.muted = true;
   player.play();
-  // 解决失焦暂停，没有结束就播放
-  setInterval(function () {
-    !video_end_flag && player.play()
-  }, 1000)
 };
 
 window.onload = main
 
+// 解决失焦暂停
 setInterval(function () {
-  var player = document.querySelector("iframe").contentWindow.
-  document.querySelector("iframe").contentWindow.
-  document.querySelector("#video_html5_api");
-  if (player.paused) {
-    main()
+  if (player && player.paused) {
+    player.play();
   }
 }, 1000)
